0c2e4aa54310d3b4b5ab98d6997e0b1df6301dd6
undeadborn <undeadborn@gmail.com>
2018-09-24 13:44:05 +0200

    code clean and readme update

--- Makefile
+++ Makefile
@@ -12,9 +12,9 @@ LDFLAGS	=
 
 EXTRA_TARGETS = EBOOT.PBP
 PSP_EBOOT_TITLE = HelloWorld
-PSP_EBOOT_ICON= ./res/ICON0.png
-PSP_EBOOT_PIC1= ./res/PIC1.png
-PSP_EBOOT_SND0= ./res/SND0.at3
+#PSP_EBOOT_ICON= ./res/ICON0.png
+#PSP_EBOOT_PIC1= ./res/PIC1.png
+#PSP_EBOOT_SND0= ./res/SND0.at3
 
 PSPSDK = $(shell psp-config --pspsdk-path)
 include $(PSPSDK)/lib/build.mak
--- src/common/callback.c
+++ src/common/callback.c
@@ -2,39 +2,26 @@
 
 static int exitRequest  = 1;
 
-int isRunning()
-{
+int isRunning() {
 	return exitRequest;
 }
 
-int exitCallback(int arg1, int arg2, void *common) 
-{ 
+int exitCallback(int arg1, int arg2, void *common) {
 	exitRequest = 0; 
-	return 0; 
+	return 0;
 } 
 
-int callbackThread(SceSize args, void *argp) 
-{ 
-	int callbackID; 
-
-	callbackID = sceKernelCreateCallback("Exit Callback", exitCallback, NULL); 
-	sceKernelRegisterExitCallback(callbackID); 
-
-	sceKernelSleepThreadCB(); 
-
-	return 0; 
+int callbackThread(SceSize args, void *argp) {
+	int callbackID = sceKernelCreateCallback("Exit Callback", exitCallback, NULL);
+	sceKernelRegisterExitCallback(callbackID);
+	sceKernelSleepThreadCB();
+	return 0;
 } 
 
-int setupExitCallback() 
-{ 
-	int threadID = 0; 
-
-	threadID = sceKernelCreateThread("Callback Update Thread", callbackThread, 0x11, 0xFA0, THREAD_ATTR_USER, 0); 
-	 
-	if(threadID >= 0) 
-	{ 
-		sceKernelStartThread(threadID, 0, 0); 
-	} 
-
+int setupExitCallback() {
+	int threadID = sceKernelCreateThread("Callback Update Thread", callbackThread, 0x11, 0xFA0, THREAD_ATTR_USER, 0);
+	if (threadID >= 0) {
+		sceKernelStartThread(threadID, 0, 0);
+	}
 	return threadID; 
 }
